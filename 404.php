c2be018923d793d2257a4919c2cb28345c3d6cd6
Bootloader1z <matthewcrew1zx@gmail.com>
2023-03-31 10:17:56 -0400

    init

--- 404.php
+++ 404.php
@@ -1,125 +1,15 @@
 <?php
-session_start();
-include('config/config.php');
-include('config/checklogin.php');
-check_login();
+if (isset($_POST['register'])) {
+  $admin_name = $_POST['admin_name'];
+  $admin_email = $_POST['admin_email'];
+  $admin_password = sha1(md5($_POST['admin_password'])); //double encrypt to increase security
+  $stmt = $mysqli->prepare("INSERT INTO rpos_admin (admin_name, admin_email, admin_password) VALUES (?, ?, ?)");
+  $stmt->bind_param('sss', $admin_name, $admin_email, $admin_password);
+  if($stmt->execute()) {
+    $success = "Registration Successful";
+  } else {
+    $err = "Error Occurred in Registration";
+  }
+}
 
-require_once('partials/_head.php');
-require_once('partials/_analytics.php');
 ?>
-
-<body>
-  <!-- Sidenav -->
-  <?php
-  require_once('partials/_sidebar.php');
-  ?>
-  <!-- Main content -->
-  <div class="main-content">
-    <!-- Top navbar -->
-    <?php
-    require_once('partials/_topnav.php');
-    ?>
-    <!-- Header -->
-    <div style="background-image: url(../admin/assets/img/theme/restro00.jpg); background-size: cover;" class="header  pb-8 pt-5 pt-md-8">
-      <span class="mask bg-gradient-dark opacity-8"></span>
-      <div class="container-fluid">
-        <div class="header-body">
-          <!-- Card stats -->
-          <div class="row">
-            <div class="col-xl-4 col-lg-6">
-              <a href="orders">
-                <div class="card card-stats mb-4 mb-xl-0">
-                  <div class="card-body">
-                    <div class="row">
-                      <div class="col">
-                      </div>
-                      <div class="col-auto">
-                        <div class="icon icon-shape bg-purple text-white rounded-circle shadow">
-                          <i class="fas fa-utensils"></i>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </a>
-            </div>
-            <div class="col-xl-4 col-lg-6">
-              <a href="orders_reports">
-                <div class="card card-stats mb-4 mb-xl-0">
-                  <div class="card-body">
-                    <div class="row">
-                      <div class="col">
-                      </div>
-                      <div class="col-auto">
-                        <div class="icon icon-shape bg-warning text-white rounded-circle shadow">
-                          <i class="fas fa-shopping-cart"></i>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </a>
-            </div>
-            <div class="col-xl-4 col-lg-6">
-              <a href="payments_reports">
-                <div class="card card-stats mb-4 mb-xl-0">
-                  <div class="card-body">
-                    <div class="row">
-                      <div class="col">
-
-                      </div>
-                      <div class="col-auto">
-                        <div class="icon icon-shape bg-green text-white rounded-circle shadow">
-                          <i class="fas fa-wallet"></i>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!-- Page content -->
-    <div class="container-fluid mt--7">
-      <div class="row mt-5">
-        <div class="col-xl-12 mb-5 mb-xl-0">
-          <div class="card shadow">
-            <div class="card-header border-0">
-              <div class="row align-items-center">
-              </div>
-            </div>
-            <div class="table-responsive">
-              <!-- Projects table -->
-             
-            </div>
-          </div>
-        </div>
-      </div>
-		
-      <div class="row mt-5">
-        <div class="col-xl-12">
-          <div class="card shadow">
-            <div class="card-header border-0">
-              <div class="row align-items-center">
-              </div>
-            </div>
-            <div class="table-responsive">
-              <!-- Projects table -->
-            </div>
-          </div>
-        </div>
-      </div>
-      <!-- Footer -->
-      <?php require_once('partials/_footer.php'); ?>
-    </div>
-  </div>
-  <!-- Argon Scripts -->
-  <?php
-  require_once('partials/_scripts.php');
-  ?>
-</body>
-
-</html>
