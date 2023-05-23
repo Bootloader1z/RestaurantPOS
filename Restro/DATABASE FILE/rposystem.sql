ed47e55921d49d88ff1f9773fdd6a9249d2605be
Bootloader1z <matthewcrew1zx@gmail.com>
2023-05-23 04:38:28 -0400

    all update fixing

--- Restro/DATABASE FILE/rposystem.sql
+++ Restro/DATABASE FILE/rposystem.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 16, 2023 at 07:32 PM
+-- Generation Time: May 23, 2023 at 10:38 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -75,12 +75,8 @@ CREATE TABLE `rpos_customers` (
 --
 
 INSERT INTO `rpos_customers` (`customer_id`, `customer_name`, `customer_phoneno`, `customer_email`, `customer_password`, `created_at`, `IP_Address`) VALUES
-('5e5e4bdc6add', 'ssz', '123123123', 'babobiaquabi1x@gmail.com', '29694a8d0ab626aaf939e2060d0895f1cbff3a4a', '2023-05-16 15:42:51.468161', '2001:4451:878c:a00:90a:6198:c668:1828'),
-('9c926d36ed16', 'Nazarene D. Laxamana', '09208217489', 'babobiaquabi@gmail.com', '29694a8d0ab626aaf939e2060d0895f1cbff3a4a', '2023-05-01 08:11:11.595176', '112.202.219.63'),
-('9d11924a7233', 'test change', '12312391239', 'testip@gmail.com', 'a73cc1d50f1d83f738dbea233af30595e00892e4', '2023-05-05 11:48:22.074426', ''),
-('b6cd9bf34ab2', 'full name x', '1231239213', 'susa1no1@gmail.com', '91534620d7789ad55a6cdc1346587d9c47875159', '2023-05-05 08:26:54.193335', '112.202.219.63'),
-('f1ecb7244bbb', 'test ip', '12312391239221', 'testip1@gmail.com', '91534620d7789ad55a6cdc1346587d9c47875159', '2023-05-05 08:26:54.193335', '112.202.219.63'),
-('fdb11f8b8217', 'test ip', '2211231', 'testip@gmail.com', '91534620d7789ad55a6cdc1346587d9c47875159', '2023-05-05 08:33:07.638473', '112.202.219.63');
+('3809daeac860', 'lhey pateno', '092222331241', 'lheypateno@gmail.com', '43f251b6bd118ed9141233676fca05e432150ad9', '2023-05-23 06:31:01.835274', ''),
+('44b3b5020e29', 'test beta', '12312391239', 'testbeta@gg.com', '6fe3ddbdc7f2bbfc91fa9fb487bd44cc7ed8e466', '2023-05-16 19:47:23.899624', '::1');
 
 -- --------------------------------------------------------
 
@@ -100,6 +96,17 @@ CREATE TABLE `rpos_orders` (
   `order_status` varchar(200) NOT NULL,
   `created_at` timestamp(6) NOT NULL DEFAULT current_timestamp(6) ON UPDATE current_timestamp(6)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `rpos_orders`
+--
+
+INSERT INTO `rpos_orders` (`order_id`, `order_code`, `customer_id`, `customer_name`, `prod_id`, `prod_name`, `prod_price`, `prod_qty`, `order_status`, `created_at`) VALUES
+('7da82c2bdc', 'QYRI-1295', '44b3b5020e29', 'test beta', 'f683ab11dc', 'terno ', '10', '10', 'Paid', '2023-05-23 07:11:16.071144'),
+('d0cbd60166', 'NUDP-9754', '44b3b5020e29', 'test beta', '3a84f4bbc4', 'Los Angeles T shirt', '25', '33', 'Paid', '2023-05-23 07:10:51.917177'),
+('dcfce974fc', 'OQFU-3987', '3809daeac860', 'lhey pateno', 'a4d40ab893', 'punk rock T shirt', '21', '10', 'Paid', '2023-05-23 06:57:19.070641'),
+('e926e3fb9a', 'GFOJ-0381', '3809daeac860', 'lhey pateno', '3a84f4bbc4', 'Los Angeles T shirt', '25', '10', 'Paid', '2023-05-23 06:48:58.394621'),
+('ea69e0c23c', 'LFMB-0742', '44b3b5020e29', 'test beta', 'c2893b7fbc', 'red floral crop top', '650', '10', 'Paid', '2023-05-23 07:11:16.080343');
 
 -- --------------------------------------------------------
 
@@ -139,6 +146,18 @@ CREATE TABLE `rpos_payments` (
   `created_at` timestamp(6) NOT NULL DEFAULT current_timestamp(6) ON UPDATE current_timestamp(6)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
+--
+-- Dumping data for table `rpos_payments`
+--
+
+INSERT INTO `rpos_payments` (`pay_id`, `pay_code`, `order_code`, `customer_id`, `pay_amt`, `pay_method`, `created_at`) VALUES
+('2693c5', 'RM2023KPNH4BWIER', 'UJYQ-5204', '44b3b5020e29', '1245', 'Cash', '2023-05-22 14:11:31.460759'),
+('33d679', 'RM2023GRT6FOCXKD', 'GFOJ-0381', '3809daeac860', '250', 'Cash', '2023-05-23 06:48:58.390033'),
+('3a7056', 'RM2023IC5FW6KG49', 'OQFU-3987', '3809daeac860', '210', 'Cash', '2023-05-23 06:57:19.067347'),
+('ad81b3', 'RM2023PAPAMDDASX', 'LFMB-0742', '44b3b5020e29', '6500', 'Cash', '2023-05-23 07:11:16.077844'),
+('d04e95', 'RM2023PAPAMDDASX', 'NUDP-9754', '44b3b5020e29', '825.00', 'Cash', '2023-05-23 07:10:51.911541'),
+('f9c840', 'RM2023PAPAMDDASX', 'QYRI-1295', '44b3b5020e29', '100', 'Cash', '2023-05-23 07:11:16.068203');
+
 -- --------------------------------------------------------
 
 --
@@ -161,24 +180,12 @@ CREATE TABLE `rpos_products` (
 --
 
 INSERT INTO `rpos_products` (`prod_id`, `prod_code`, `prod_name`, `prod_img`, `prod_desc`, `prod_price`, `quantity`, `created_at`) VALUES
-('3adfdee116', 'HIPF-5346', 'Enchiladas', 'enchiladas.jpg', 'An enchilada is a corn tortilla rolled around a filling and covered with a savory sauce. Originally from Mexican cuisine, enchiladas can be filled with various ingredients, including meats, cheese, beans, potatoes, vegetables, or combinations', '10', 0, '2023-05-16 17:25:07.556230'),
-('3d19e0bf27', 'EMBH-6714', 'Cincinnati Chili', 'cincinnatichili.jpg', 'Cincinnati chili is a Mediterranean-spiced meat sauce used as a topping for spaghetti or hot dogs; both dishes were developed by immigrant restaurateurs in the 1920s. In 2013, Smithsonian named one local chili parlor one of the \"20 Most Iconic Food Destinations in America\".', '9', 0, '2023-05-16 17:27:15.595112'),
-('5d66c79953', 'GOEW-9248', 'Cheese Curd', 'cheesecurd.jpg', 'Cheese curds are moist pieces of curdled milk, eaten either alone or as a snack, or used in prepared dishes. These are chiefly found in Quebec, in the dish poutine, throughout Canada, and in the northeastern, midwestern, mountain, and Pacific Northwestern United States, especially in Wisconsin and Minnesota.', '6', 0, '2023-05-16 17:29:45.484865'),
-('826e6f687f', 'AYFW-2683', 'Margherita Pizza', 'margherita-pizza0.jpg', 'Pizza margherita, as the Italians call it, is a simple pizza hailing from Naples. When done right, margherita pizza features a bubbly crust, crushed San Marzano tomato sauce, fresh mozzarella and basil, a drizzle of olive oil, and a sprinkle of salt.', '12', 100, '2023-05-16 17:14:20.561834'),
-('86f1fe3521', 'RWOI-6702', 'jhonny jhonny', '', '221', '55', 100, '2023-05-16 17:14:22.018559'),
-('97972e8d63', 'CVWJ-6492', 'Irish Coffee', 'irishcoffee.jpg', 'Irish coffee is a caffeinated alcoholic drink consisting of Irish whiskey, hot coffee, and sugar, stirred, and topped with cream The coffee is drunk through the cream', '11', 100, '2023-05-16 17:14:24.012270'),
-('a419f2ef1c', 'EPNX-3728', 'Chicken Nugget', 'chicnuggets.jpeg', 'A chicken nugget is a food product consisting of a small piece of deboned chicken meat that is breaded or battered, then deep-fried or baked. Invented in the 1950s, chicken nuggets have become a very popular fast food restaurant item, as well as widely sold frozen for home use', '5', 100, '2023-05-16 17:14:25.430348'),
-('a5931158fe', 'ELQN-5204', 'Pulled Pork', 'pulledprk.jpeg', 'Pulled pork is an American barbecue dish, more specifically a dish of the Southern U.S., based on shredded barbecued pork shoulder. It is typically slow-smoked over wood; indoor variations use a slow cooker. The meat is then shredded manually and mixed with a sauce', '8', 100, '2023-05-16 17:14:26.533851'),
-('b2f9c250fd', 'XNWR-2768', 'Strawberry Rhubarb Pie', '', 'Rhubarb pie is a pie with a rhubarb filling. Popular in the UK, where rhubarb has been cultivated since the 1600s, and the leaf stalks eaten since the 1700s. Besides diced rhubarb, it almost always contains a large amount of sugar to balance the intense tartness of the plant', '7', 100, '2023-05-16 17:14:27.553843'),
-('bd200ef837', 'HEIY-6034', 'Turkish Coffee', 'turkshcoffee.jpg', 'Turkish coffee is a style of coffee prepared in a cezve using very finely ground coffee beans without filtering.', '8', 100, '2023-05-16 17:14:28.591663'),
-('d57cd89073', 'ZGQW-9480', 'Country Fried Steak', 'country_fried_stk.jpg', 'Chicken-fried steak, also known as country-fried steak or CFS, is an American breaded cutlet dish consisting of a piece of beefsteak coated with seasoned flour and either deep-fried or pan-fried. It is sometimes associated with the Southern cuisine of the United States.', '10', 100, '2023-05-16 17:14:31.153271'),
-('d9aed17627', 'FIKD-9703', 'Crab Cake', 'crabcakes.jpg', 'A crab cake is a variety of fishcake that is popular in the United States. It is composed of crab meat and various other ingredients, such as bread crumbs, mayonnaise, mustard, eggs, and seasonings. The cake is then sautÃ©ed, baked, grilled, deep fried, or broiled.', '16', 100, '2023-05-16 17:14:33.259946'),
-('e2195f8190', 'HKCR-2178', 'Carbonara', 'carbonaraimgre.jpg', 'Carbonara is an Italian pasta dish from Rome made with eggs, hard cheese, cured pork, and black pepper. The dish arrived at its modern form, with its current name, in the middle of the 20th century. The cheese is usually Pecorino Romano, Parmigiano-Reggiano, or a combination of the two.', '16', 100, '2023-05-16 17:14:36.182512'),
-('e2af35d095', 'IDLC-7819', 'Pepperoni Pizza', 'peperopizza.jpg', 'Pepperoni is an American variety of spicy salami made from cured pork and beef seasoned with paprika or other chili pepper. Prior to cooking, pepperoni is characteristically soft, slightly smoky, and bright red. Thinly sliced pepperoni is one of the most popular pizza toppings in American pizzerias.', '7', 100, '2023-05-16 17:14:37.425797'),
-('e769e274a3', 'AHRW-3894', 'Frappuccino', 'frappuccino.jpg', 'Frappuccino is a line of blended iced coffee drinks sold by Starbucks. It consists of coffee or crÃ¨me base, blended with ice and ingredients such as flavored syrups and usually topped with whipped cream and or spices.', '3', 100, '2023-05-16 17:14:38.346714'),
-('ec18c5a4f0', 'PQFV-7049', 'Corn Dogs', 'corndog.jpg', 'A corn dog is a sausage on a stick that has been coated in a thick layer of cornmeal batter and deep fried. It originated in the United States and is commonly found in American cuisine', '4', 100, '2023-05-16 17:14:39.401913'),
-('f4ce3927bf', 'EAHD-1980', 'Hot Dog', 'hotdog0.jpg', 'A hot dog is a food consisting of a grilled or steamed sausage served in the slit of a partially sliced bun. The term hot dog can also refer to the sausage itself. The sausage used is a wiener or a frankfurter. The names of these sausages also commonly refer to their assembled dish.', '4', 100, '2023-05-16 17:14:40.419170'),
-('f9c2770a32', 'YXLA-2603', 'Whipped Milk Shake', 'milkshake.jpeg', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc,', '8', 100, '2023-05-16 17:14:43.612027');
+('3a84f4bbc4', 'KFPM-2437', 'Los Angeles T shirt', '71gpR-r22HL._AC_UL320_.jpg', 'Los Angeles T shirt for men', '25', 0, '2023-05-23 07:09:15.695444'),
+('9db058e0d8', 'BOWS-9578', 'Winter pajamas for women', '81dMryMuQFL._AC_UY350_.jpg', 'winter pajama for women', '17', 85, '2023-05-22 14:49:47.856809'),
+('a4d40ab893', 'YTDR-1607', 'punk rock T shirt', 'B1OGJ8t+8ZS._CLa_2140,2000_71GHv+xlqHL.png_0,0,2140,2000+0.0,0.0,2140.0,2000.0_AC_SL1500_.png', 'punk rock T shirt\r\nsmall to extra large ', '21', 74, '2023-05-23 06:56:19.877414'),
+('af454918af', 'GTPE-9278', 'Black leather jacket ', '', 'Black Leather Jacket\r\nFree Size ', '15', 74, '2023-05-22 14:46:40.565437'),
+('c2893b7fbc', 'CP-001', 'red floral crop top', 'images (1).jpeg', 'a red floral coquette crop top\r\nSIZE FREE', '650', 38, '2023-05-23 07:08:45.473170'),
+('f683ab11dc', 'ZFRO-9671', 'terno ', '41tzCuexOLS._SS400_.jpg', 'terno with variety of colors ', '10', 536, '2023-05-23 07:11:07.483016');
 
 -- --------------------------------------------------------
 
@@ -200,7 +207,7 @@ CREATE TABLE `rpos_staff` (
 --
 
 INSERT INTO `rpos_staff` (`staff_id`, `staff_name`, `staff_number`, `staff_email`, `staff_password`, `created_at`) VALUES
-(2, 'Cashier Trevor', 'QEUY-9042', 'cashier@mail.com', '903b21879b4a60fc9103c3334e4f6f62cf6c3a2d', '2022-09-04 16:11:30.581882');
+(4, 'CASHIER', 'CVND-4360', 'cash@cash.com', '0dc777c2e97fbe36333e52d2aa2d52b9f1706a27', '2023-05-23 06:54:14.824614');
 
 -- --------------------------------------------------------
 
@@ -304,7 +311,7 @@ ALTER TABLE `rpos_pass_resets`
 -- AUTO_INCREMENT for table `rpos_staff`
 --
 ALTER TABLE `rpos_staff`
-  MODIFY `staff_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `staff_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
